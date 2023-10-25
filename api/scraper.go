6a95fecaf270af79fa3b3b21cba2b0085b7f010e
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-10-25 21:57:47 +0300

    scraper completed

--- api/scraper.go
+++ api/scraper.go
@@ -1,13 +1,15 @@
 package api
 
 import (
+	"context"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/Jhnvlglmlbrt/rss-aggregator/internal/database"
 )
 
-func startScraping(
+func StartScraping(
 	db *database.Queries, // connection to database
 	concurrency int, // number of diff goroutines to do the scraping
 	timeBetweenRequest time.Duration, // time between each request to go scrape a new rss feed
@@ -15,6 +17,43 @@ func startScraping(
 	log.Printf("Scraping on %v goroutines every %s duraiton", concurrency, timeBetweenRequest)
 	ticker := time.NewTicker(timeBetweenRequest)
 	for ; ; <-ticker.C {
+		feeds, err := db.GetNextFeedsToFetch(
+			context.Background(),
+			int32(concurrency), // limit
+		)
+		if err != nil {
+			log.Println("error fetching feeds: ", err)
+			continue
+		}
 
+		wg := &sync.WaitGroup{}
+		for _, feed := range feeds {
+			wg.Add(1)
+
+			go scrapeFeed(db, wg, feed)
+		}
+		wg.Wait()
 	}
+}
+
+func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
+	defer wg.Done()
+
+	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Println("Error marking feed as fetched")
+		return
+	}
+
+	rssFeed, err := UrlToFeed(feed.Url)
+	if err != nil {
+		log.Println("Error fetching feed:", err)
+		return
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		log.Println("Found post", item.Title, "on feed ", feed.Name)
+	}
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
+
 }
