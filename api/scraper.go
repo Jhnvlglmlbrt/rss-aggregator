777abd5274cad2e1f4c7d749496242e8968f66b2
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-10-25 23:54:03 +0300

    updated scraper with creating post in db

--- api/scraper.go
+++ api/scraper.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/Jhnvlglmlbrt/rss-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func StartScraping(
@@ -52,7 +55,36 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed ", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("error parsing date %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		// log.Println("Found post", item.Title, "on feed ", feed.Name)
+		_, err = db.CreatePost(context.Background(),
+			database.CreatePostParams{
+				ID:          uuid.New(),
+				CreatedAt:   time.Now().UTC(),
+				UpdatedAt:   time.Now().UTC(),
+				Title:       item.Title,
+				Description: description,
+				PublishedAt: pubAt,
+				Url:         item.Link,
+				FeedID:      feed.ID,
+			})
+		if err != nil {
+			if strings.Contains(err.Error(), "повторяющееся значение ключа") {
+				continue
+			}
+			log.Println("error creating post: ", err)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
